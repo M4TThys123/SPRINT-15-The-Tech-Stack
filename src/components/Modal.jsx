b5f3280c8848b8d1d08e3f131b6ff5ee60ca5636
M4TThys123 <70703948+M4TThys123@users.noreply.github.com>
2022-11-26 14:39:53 +0100

    ⏪️ Changed the modal #5

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,10 +4,10 @@ function Modal() {
   return (
     <div>
       <div className="modal">
-        <p className="modal__title">Are you sure?</p>
+        <p className="modal__title">Weet je het zeker?</p>
         <div className="modal__buttons">
-          <button className="btn btn__cancel">Cancel</button>
-          <button className="btn btn__primary">Confirm</button>
+          <button className="btn btn__cancel">Annuleren</button>
+          <button className="btn btn__primary">Bevestigen</button>
         </div>
       </div>
       <div className="backdrop" />
